95d6f94d5a008b69a89171052d8fc9595d89fa13
Ubuntu <ubuntu@ip-172-31-30-7.eu-north-1.compute.internal>
2024-09-17 11:45:36 +0000

    changed to Private subnet for EKS

--- main.tf
+++ main.tf
@@ -41,7 +41,7 @@ module "eks" {
   }
 
   vpc_id                   = module.vpc.vpc_id
-  subnet_ids               = module.vpc.public_subnets
+  subnet_ids               = module.vpc.private_subnets
   control_plane_subnet_ids = module.vpc.private_subnets
 
   eks_managed_node_group_defaults = {
